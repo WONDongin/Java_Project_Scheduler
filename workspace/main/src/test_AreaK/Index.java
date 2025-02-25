e365cd859f7e6795e29c5a28a89b67eefb6859ea
yerok67 <yerock81@gmail.com>
2025-02-25 16:06:22 +0900

    Update Index.java
    ㄹㅇ

--- workspace/main/src/test_AreaK/Index.java
+++ workspace/main/src/test_AreaK/Index.java
@@ -2,6 +2,7 @@ package test_AreaK;
 // K_Test_Area
 public class Index {
 	public static void main(String[] args) {
-		System.out.println("말은 쉽지, 코드를 보여줘.");
+		System.out.println("말은 쉽지, 코드를 보여줘.1");
+		System.out.println("ㄹㅇ");
 	}
 }
