269813b8fc4a3df392b152ca497598ae0076c6af
yerok67 <yerock81@gmail.com>
2025-02-25 18:32:40 +0900

    Update Event.java

--- workspace/main/src/test_AreaY/Event.java
+++ workspace/main/src/test_AreaY/Event.java
@@ -1,22 +1,28 @@
-package test_AreaY;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
-class Event{
-	String name;
+class Event implements Comparable<Event>{
+	String eventname;
 	String content;
 	String feventdate;
 	String leventdate;
-	
-	public Event(String name, String content, String feventdate, String leventdate) {
-		this.name = name;
+	public Event(String eventname, String content, String feventdate, String leventdate) {
+		this.eventname = eventname;
 		this.content = content;
 		this.feventdate = feventdate;
 		this.leventdate = leventdate;
 	}
 	public String toString() {
-		return "[제목]: " + name + "\n" + "[기간]: " + feventdate + "~" 
+		return "[제목]: " + eventname + "\n" + "[기간]: " + feventdate + "~" 
 	            + leventdate + "\n[상세]: " + content + "\n";		
 	}
-	public int compareTo(Event e) {
-		return feventdate.compareTo(e.feventdate);
+	public int compareTo(Event e){
+		//비교를 위해 입력받은 날짜를 정수형 변환
+		SimpleDateFormat sf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+			try {
+				return sf.parse(feventdate).compareTo(sf.parse(leventdate));
+			} catch (ParseException e1) {
+				return 0;
+			}
 	}
 }
